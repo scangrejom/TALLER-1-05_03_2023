b857d8b9c9dd5f3ba2938b23da4cc8e6b15ea5c1
Stephanie <stephanie.scm@gmail.com>
2023-03-05 21:17:47 -0500

    Taller 1 decisiones

---
+++ programa.js
@@ -0,0 +1,19 @@
+//Taller 1 05_03_2023 
+//1. Definir dos variables, asignarles un valor y utilizando 
+//decisiones imprimir en la consola la que tenga menor valor
+
+let a = 90;
+let b = 500;
+
+if(a<b){console.log(a)} else{console.log(b)};
+
+// 2. Definir tres variables, asignarles valores y utilizar decisiones para imprimir
+// en consola la que tenga el mayor valor
+
+let c= 45
+let d= 100
+let e= 433
+
+if(c>d && c>e){console.log(c)}
+if(d>c && d>e){console.log(d)};
+if(e>c && e>c){console.log(e)};
